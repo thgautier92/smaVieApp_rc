26a492a469a17f6ed0c8982d1ca57b3cef15b4c4
Thierry GAUTIER <th.gautier92@gmail.com>
2016-10-12 12:16:16 +0200

    BUg corrections

--- src/providers/comon/info-device.ts
+++ src/providers/comon/info-device.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { NavController, Platform } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
 /*
